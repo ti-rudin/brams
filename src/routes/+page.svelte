e98e6731b8ef767b7fe8c245c31cec2062f597a8
A.Rudin <a.rudin@berg.ru>
2025-02-05 14:26:45 +0300

    `Refactor navigation menu in +page.svelte: remove "Доставка" and "Цены" links, add "Качество" link`

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -163,16 +163,10 @@
               <a
                 href="#delivery"
                 class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                on:click={toggleMenu}>Доставка</a
+                on:click={toggleMenu}>Качество</a
               >
             </li>
-            <li>
-              <a
-                href="#price"
-                class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                on:click={toggleMenu}>Цены</a
-              >
-            </li>
+     
             <li>
               <a
                 href="#contacts"
@@ -444,7 +438,7 @@
     >
       <div class="items-center col-span-2 mb-4">
         <p
-          class="text-2xl font-medium dark:text-white text-center"
+          class="text-2xl font-bold dark:text-white text-center"
         >
           Порядок отгрузки
         </p>
